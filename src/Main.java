fa5eecd431f55db6383d7bc7a02d1fe62f4e1d3d
victor <victor@ieselcaminas.org>
2022-12-12 15:07:15 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException  {
+        SortedTextFile s = new SortedTextFile("test.txt");
+        s.put("ggggggg");
+        s.put("bbbbbbb");
+        s.put("aaaaaaa");
+        s.put("ccccccc");
+        s.put("ddddddd");
+        s.print();
+        //s.removeElementAt(2);
+        //s.removeElementAt(3);
+        // s.emptyFile();
+    }
+}
---
+++ src/SortedTextFile.java
@@ -0,0 +1,148 @@
+import java.io.*;
+
+public class SortedTextFile {
+    private String filename;
+
+    public SortedTextFile(String filename) throws IOException {
+        this.filename = filename;
+        File file = new File(filename);
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void put(String newLine) throws IOException {
+        File file = new File(filename);
+        File temp = new File("temp.txt");
+        BufferedReader input = null;
+        PrintWriter out = null;
+
+        try {
+            input = new BufferedReader(new FileReader(file));
+            out = new PrintWriter(new FileWriter(temp));
+            String line;
+            boolean alreadyInserted = false;
+            while ((line = input.readLine()) != null) {
+                if (!alreadyInserted && line.compareTo(newLine) > 0) {
+                    out.println(newLine);
+                    alreadyInserted = true;
+                }
+                out.println(line);
+            }
+            if (!alreadyInserted) {
+                out.println(newLine);
+            }
+            file.delete();
+            temp.renameTo(file);
+        } finally {
+            if (input != null) {
+                input.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
+
+    public int getNumElements() throws IOException {
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new FileReader(filename));
+            String line;
+            int counter = 0;
+            while ((line = in.readLine()) != null) {
+                counter++;
+            }
+            return counter;
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+        }
+    }
+
+    public String getElementAt(int numLine) throws IOException {
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new FileReader(filename));
+            String line;
+            int counter = 0;
+            while ((line = in.readLine()) != null) {
+                counter++;
+                if (counter == numLine) {
+                    return line;
+                }
+            }
+            return null;
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+        }
+    }
+
+    public void removeElementAt(int numLine) throws IOException {
+        File file = new File(filename);
+        File temp = new File("temp.txt");
+        BufferedReader in = null;
+        PrintWriter out = null;
+        try {
+            in = new BufferedReader(new FileReader(file));
+            out = new PrintWriter(new FileWriter(temp));
+            String line;
+            int counter = 0;
+            while ((line = in.readLine()) != null) {
+                counter++;
+                if (counter != numLine) {
+                    out.println(line);
+                }
+            }
+            file.delete();
+            temp.renameTo(file);
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
+
+    public boolean isEmpty() throws IOException {
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new FileReader(filename));
+            return in.readLine() == null;
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+        }
+    }
+
+    public void emptyFile() throws IOException {
+        File file = new File(filename);
+        file.delete();
+        file.createNewFile();
+    }
+
+    public void print() throws IOException {
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new FileReader(filename));
+            String line;
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+        }
+    }
+}
